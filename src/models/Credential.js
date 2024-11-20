1b8c60a5e16d7a3cb197f7ffaed90565f19a7f9f
sinbumu <sinbumu@gmail.com>
2024-11-20 15:38:07 +0900

    Update Credential.js
    data model 수정

--- src/models/Credential.js
+++ src/models/Credential.js
@@ -27,6 +27,18 @@ const credentialSchema = new mongoose.Schema({
       type: String,
       required: true
    },
+   issuanceTime: {  // 발급 시각 필드 추가
+      type: Number,
+      required: true
+   },
+   expirationTime: {  // 만료 시각 필드 추가
+      type: Number,
+      required: true
+   },
+   optionalData: {  // 추가 데이터 필드 추가
+      type: String,
+      default: null
+   },
    isDeleted: {  // 논리 삭제 여부
       type: Boolean,
       default: false
